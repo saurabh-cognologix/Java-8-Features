9fb658958233525cf501341139e740a700e148c0
Saurabh <target4ever101@gmail.com>
2022-09-15 17:45:40 +0530

    java 8 iteration methods

--- Iteration/Iteration1.java
+++ Iteration/Iteration1.java
@@ -12,10 +12,16 @@ public class Iteration1 {
         list.add(200);
         list.add(400);
         list.add(500);
+
+
         System.out.println("------------Iterating by passing lambda expression--------------");
         list.forEach((n)-> System.out.println(n));
+
+
         System.out.println("------------Iterating by passing method reference---------------");
         list.forEach(System.out::println);
+
+
         System.out.println("------------Iterating by passing method reference(forEachOrdered)---------------");
         list.stream().forEachOrdered(System.out::println);
     }
