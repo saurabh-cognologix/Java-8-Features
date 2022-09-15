bf51c78b041993e35b9df465ae1b4c6fac8354dc
Saurabh <target4ever101@gmail.com>
2022-09-15 17:44:28 +0530

    java 8 iteration method

---
+++ Iteration/Iteration1.java
@@ -0,0 +1,22 @@
+package StreamAPIJava8.Iteration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Iteration1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(100);
+        list.add(300);
+        list.add(200);
+        list.add(400);
+        list.add(500);
+        System.out.println("------------Iterating by passing lambda expression--------------");
+        list.forEach((n)-> System.out.println(n));
+        System.out.println("------------Iterating by passing method reference---------------");
+        list.forEach(System.out::println);
+        System.out.println("------------Iterating by passing method reference(forEachOrdered)---------------");
+        list.stream().forEachOrdered(System.out::println);
+    }
+}
